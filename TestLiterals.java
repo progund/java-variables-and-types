c29ee6286dd652fdcf52eb54835f9965cde3f488
Rikard Fröberg <rikard@morus.se>
2018-11-18 17:28:18 +0100

    Class for the exercise on Java primitive types

---
+++ TestLiterals.java
@@ -0,0 +1,22 @@
+/**
+ * This class is only used for learning about what
+ * the compiler says about various types of literals
+ * and expressions.
+ *
+ * It is not very useful or typical for a Java class
+ * in any regard. But it is the minimum you need
+ * to enter some variables and assigning them values
+ * of some type, to see what the compiler can teach
+ * you about types.
+ */
+public class TestLiterals {
+  int distanceToMoon = 384400000; // meters
+  double earthGravity = 9.807; // m/s^2
+  //int IQ = 65L; // Appending an 'L' to an integer
+                  // literal expression forces it
+                  // to be of type long
+
+  int distance = 50; // km
+  double speed = 100.0; // km/h
+  double time = distance / speed; // hours
+}
